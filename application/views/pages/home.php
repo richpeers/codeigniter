6aaf3bcaaa5d95068e3902b7938bc1f8e4b8aeb0
Rich Peers <richpeers1@gmail.com>
2019-07-28 19:14:04 +0100

    static pages, css and template

---
+++ application/views/pages/home.php
@@ -0,0 +1,7 @@
+<main role="main" class="container">
+    <div class="jumbotron">
+        <h1>CodeIgniter</h1>
+        <p class="lead">Quick exercise to build a basic task app with <a href="https://www.codeigniter.com/" target="_blank">CodeIgniter</a></p>
+        <a class="btn btn-outline-primary" href="https://www.codeigniter.com/docs" target="_blank" role="button">View docs</a>
+    </div>
+</main>
---
+++ application/views/templates/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title><?php echo $title ?></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <a class="navbar-brand" href="/">Tasks</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
