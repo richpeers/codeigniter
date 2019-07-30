41f04fb358e6a7c142dde5bbd682bb530d0c080e
Rich Peers <richpeers1@gmail.com>
2019-07-30 04:13:35 +0100

    images

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,9 +1,13 @@
 <main role="main" class="container">
-	<div class="jumbotron">
-		<h1>CodeIgniter</h1>
-		<p class="lead">
-			Quick exercise to build a basic app with <a href="https://www.codeigniter.com/" target="_blank">CodeIgniter</a>
-		</p>
-		<a class="btn btn-lg btn-outline-primary pl-4 pr-4" href="/news">News</a>
-	</div>
+    <div class="jumbotron d-flex align-items-center">
+
+        <img class="float-left mr-4" src="/img/ci-logo-big.png" height="140px;" alt="CodeIgniter">
+        <div>
+            <p class="lead">
+                Quick exercise to build a basic app with <a href="https://www.codeigniter.com/" target="_blank">CodeIgniter</a>
+            </p>
+            <a class="btn btn-outline-primary pl-4 pr-4" href="/news">News</a>
+        </div>
+
+    </div>
 </main>
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,44 +1,51 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-	<div class="container">
+    <div class="container">
 
-		<a class="navbar-brand" href="/">Home</a>
+        <!--		<a class="navbar-brand" href="/">Home</a>-->
 
-		<button class="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarCollapse"
-				aria-controls="navbarCollapse"
-				aria-expanded="false"
-				aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+        <a class="navbar-brand d-flex align-items-center" href="/">
+            <img class="mr-1" src="/img/ci-logo-white.png" height="35px" alt="CodeIgniter">
+            <span><strong><i>CodeIgniter</i></strong></span>
+        </a>
 
-		<div class="collapse navbar-collapse" id="navbarCollapse">
+        <button class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarCollapse"
+                aria-controls="navbarCollapse"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-			<div class="navbar-nav">
-				<a class="nav-item nav-link" href="/news">News</a>
-			</div>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
 
-			<div class="navbar-nav mr-auto">
-				<a class="nav-item nav-link" href="https://github.com/richpeers/codeigniter" target="_blank"><i class="fab fa-github fa-2x"></i></a>
-				<a class="nav-item nav-link" href="https://www.linkedin.com/in/richpeers" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
-			</div>
-			<ul class="navbar-nav ml-auto">
-				<?php if ($auth->guest()) { ?>
-					<a class="btn btn-outline-light mr-3" href="/login">Login</a>
-					<a class="btn btn-light" href="/register">Register</a>
-				<?php } ?>
+            <div class="navbar-nav  mr-auto">
+                <a class="nav-item nav-link" href="/news">News</a>
+                <a class="nav-item nav-link" href="https://github.com/richpeers/codeigniter" target="_blank"><i class="fab fa-github fa-lg"></i></a>
+                <a class="nav-item nav-link" href="https://www.linkedin.com/in/richpeers" target="_blank"><i class="fab fa-linkedin fa-lg"></i></a>
+            </div>
 
-				<?php if ($auth->check()) { ?>
-					<span class="navbar-text mr-3">
+            <div class="navbar-nav">
+
+
+            </div>
+            <ul class="navbar-nav ml-auto">
+                <?php if ($auth->guest()) { ?>
+                    <a class="btn btn-sm btn-outline-light mr-3" href="/login">Login</a>
+                    <a class="btn btn-sm btn-light" href="/register">Register</a>
+                <?php } ?>
+
+                <?php if ($auth->check()) { ?>
+                    <span class="navbar-text mr-3">
 						<?php echo $auth->name() ?>
 					</span>
-					<a class="btn btn-outline-light" href="/logout">Logout</a>
-				<?php } ?>
+                    <a class="btn btn-outline-light" href="/logout">Logout</a>
+                <?php } ?>
 
-			</ul>
+            </ul>
 
-		</div>
+        </div>
 
-	</div>
+    </div>
 </nav>
